4b0a24f2e0e892c4016ee698f6faee8912e95d25
HassanAfzal123 <hassan_afzal6@yahoo.com>
2022-12-21 02:49:45 +0100

    Fixed cypress test case for finish game

--- cypress/e2e/finish_a_game.cy.js
+++ cypress/e2e/finish_a_game.cy.js
@@ -15,12 +15,24 @@ describe('Edit a game', () => {
             })
         })
 
+        let homeTeamScore = await new Promise((resolve) => {
+            cy.get('[data-testid="scoreboard_match_0_hometeam_score"]').then(($elem)=>{
+                resolve($elem.text())
+            })
+        })
+
+        let awayTeamScore = await new Promise((resolve) => {
+            cy.get('[data-testid="scoreboard_match_0_awayteam_score"]').then(($elem)=>{
+                resolve($elem.text())
+            })
+        })
+
         //Click finish button
         await cy.get('[data-testid="scoreboard_match_0_finish"]')
         .click();
 
         // Using regex to match the string
-        let regex = new RegExp(homeTeam + "\s*30\s*-\s*" + awayTeam + "\s*35");
+        let regex = new RegExp(homeTeam + "\s*" + homeTeamScore + "\s*-\s*" + awayTeam + "\s*" + awayTeamScore);
         cy.get('[data-testid="scoreboard"]').invoke('text').should('not.match', regex)
     });    
 });
